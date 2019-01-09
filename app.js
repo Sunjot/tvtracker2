aee86aad03d2a05107d76d1de2ffbe1314fbd377
Sunjot Singh <sunjot@uoguelph.ca>
2019-01-09 02:36:37 -0500

    added mongoose connection and dotenv

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
-app.use(bodyParser.json());
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 const port = 3000;
+const app = express();
+
+app.use(bodyParser.json()); // Used to parse JSON request body
+
+dotenv.config(); // used to load env variables from .env file
+
+// Connection to mongoDB Atlas Cluster
+var mongoURL = "mongodb+srv://sunjotsingh:" + process.env.MDBPASS + "@tvtracker-bykmv.mongodb.net/test?retryWrites=true";
+mongoose.connect(mongoURL, {useNewUrlParser: true});
+
+var db = mongoose.connection; // Connection established
+db.once('open', () => {
+  console.log("Connection established to MongoDB Cluster");
+});
 
 app.post('/api/login', (req, res) => {
   console.log(req.body);
