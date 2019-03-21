fb74327fc084281ee19a5f8a662c0d3c2a0fc380
Sunjot Singh <sunjot@uoguelph.ca>
2019-03-21 02:55:38 -0400

    worked on getting genre filtering listed

--- client/Components/Collection.js
+++ client/Components/Collection.js
@@ -11,9 +11,10 @@ class Collection extends React.Component {
   constructor() {
     super();
     this.state = {
-      collection: "",
-      suggestions: [],
-      addId: []
+      collection: "", // for loading all shows in the collection on initial render
+      suggestions: [], // to store all the suggestions from 3rd party api call
+      addId: [], // for keeping track of which shows in the suggestion list are part of the collection
+      filters: []
     }
   }
 
@@ -26,14 +27,22 @@ class Collection extends React.Component {
       return res.json();
     }).then((res) => {
       if (res.length > 0) {
-        this.setState({
-          collection: res
+        this.setState({collection: res});
+
+        // go through all the genres and add them to the state array if not already there
+        var filterArray = this.state.filters;
+        res.map((eachShow, x) => {
+          eachShow.genres.map((genre, i) => {
+            if (!filterArray.includes(genre)) {
+              filterArray.push(genre);
+            }
+          });
         });
+
+        this.setState({filters: filterArray});
       }
       else {
-        this.setState({
-          collection: []
-        });
+        this.setState({collection: []});
       }
     });
   }
@@ -74,14 +83,17 @@ class Collection extends React.Component {
       headers: {'Content-Type': 'application/json'}
     }).then((res) => {
       if (res.status === 200) {
-        var index = this.state.addId.indexOf(showID);
-        var index2 = this.state.collection.map((sh, x) => {
+        var addIndex = this.state.addId.indexOf(showID);
+        // Since the collection is an array of objects, slightly more complex to find the index of an id
+        var collIndex = this.state.collection.map((sh, x) => {
           return sh.showID
         }).indexOf(showID);
 
         this.setState({
-          addId: [...this.state.addId.slice(0,index), ...this.state.addId.slice(index+1)],
-          collection: [...this.state.collection.slice(0, index2), ...this.state.collection.slice(index2+1)]
+          // Show removed from this array so the add/remove icons update accordingly
+          addId: [...this.state.addId.slice(0,addIndex), ...this.state.addId.slice(addIndex+1)],
+          // When removing a show, we also need to do so from the collection so the page updates accordingly
+          collection: [...this.state.collection.slice(0, collIndex), ...this.state.collection.slice(collIndex+1)]
         });
       }
     });
@@ -126,14 +138,30 @@ class Collection extends React.Component {
         }
         {Array.isArray(this.state.collection) && this.state.collection.length > 0 &&
           <div id="coll-cont">
-            {this.state.collection.map((show, x) => {
-              return(
-                <div id="posterDiv" key={x} onClick={() => this.removeShow(show.showID)}>
-                  <Remove id="plus-icon" size={30} color="White"/>
-                  <img src={"https://image.tmdb.org/t/p/w1280" + show.poster}></img>
+            <div id="filter-section">
+              {this.state.filters.length > 3 &&
+                <div id="filter-list">
+                {this.state.filters.map((filter, x) => {
+                  return(
+                    <p>{filter}</p>
+                  )
+                })}
                 </div>
-              );
-            })}
+              }
+              {this.state.filters.length < 4 &&
+                <div>Add more shows to be able to filter your collection by genres!</div>
+              }
+            </div>
+            <div id="coll-row">
+              {this.state.collection.map((show, x) => {
+                return(
+                  <div id="posterDiv" key={x} onClick={() => this.removeShow(show.showID)}>
+                    <Remove id="plus-icon" size={30} color="White"/>
+                    <img src={"https://image.tmdb.org/t/p/w1280" + show.poster}></img>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         }
       </div>
