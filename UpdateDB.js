42758314082420ee6812a218f511ee9268d45f0d
Sunjot Singh <sunjot@uoguelph.ca>
2019-04-02 21:04:18 -0400

    added timeouts to api calls due to limits

--- UpdateDB.js
+++ UpdateDB.js
@@ -23,41 +23,54 @@ var j = schedule.scheduleJob('* 4 * * *', () => {
   updateAirDates();
 });
 
+updateAirDates();
+
+async function getShow(show) {
+  console.log("Fire");
+  var tvURL = "https://api.themoviedb.org/3/tv/" + show.showID + "?api_key=" + process.env.TMDBKEY;
+  fetch(tvURL, {
+    method: 'get'
+  }).then((res) => {
+    return res.json();
+  }).then((data) => {
+    // If the episode has another air date, update nextAir in the DB with the new date/name
+    if (data.next_episode_to_air) {
+
+      var nextDate = moment(data.next_episode_to_air.air_date);
+      var currentDate = moment(show.nextAir.get('date'));
+
+      // Compare episode air dates to see if they require updating
+      if (currentDate.isBefore(nextDate)) {
+        Show.findOne({showID: show.showID}, (error, sh) => {
+          sh.nextAir.set('date', data.next_episode_to_air.air_date);
+          sh.nextAir.set('name', data.next_episode_to_air.name);
+          sh.save();
+        });
+        console.log("Updated " + data.original_name + " to " + data.next_episode_to_air.air_date);
+      }
+    }
+    // If the episode has no more air dates, update nextAir in the DB with an undefined date/name
+    if (!data.next_episode_to_air) {
+      Show.findOne({showID: show.showID}, (error, sh) => {
+        sh.nextAir.set('date', undefined);
+        sh.nextAir.set('name', undefined);
+        sh.save();
+      });
+    }
+  });
+}
+
 function updateAirDates() {
 
   Show.find({}, (err, shows) => {
-    shows.map(show => {
-      var tvURL = "https://api.themoviedb.org/3/tv/" + show.showID + "?api_key=" + process.env.TMDBKEY;
-      fetch(tvURL, {
-        method: 'get'
-      }).then((res) => {
-        return res.json();
-      }).then((data) => {
-        // If the episode has another air date, update nextAir in the DB with the new date/name
-        if (data.next_episode_to_air) {
+    shows.slice(0, 40).map(show => getShow(show));
 
-          var nextDate = moment(data.next_episode_to_air.air_date);
-          var currentDate = moment(show.nextAir.get('date'));
-
-          // Compare episode air dates to see if they require updating
-          if (currentDate.isBefore(nextDate)) {
-            Show.findOne({showID: show.showID}, (error, sh) => {
-              sh.nextAir.set('date', data.next_episode_to_air.air_date);
-              sh.nextAir.set('name', data.next_episode_to_air.name);
-              sh.save();
-            });
-            console.log("Updated " + data.original_name + " to " + data.next_episode_to_air.air_date);
-          }
-        }
-        // If the episode has no more air dates, update nextAir in the DB with an undefined date/name
-        if (!data.next_episode_to_air) {
-          Show.findOne({showID: show.showID}, (error, sh) => {
-            sh.nextAir.set('date', undefined);
-            sh.nextAir.set('name', undefined);
-            sh.save();
-          });
-        }
-      });
-    });
+    // API limit of 40 every 10 seconds puts restriction on number of API calls
+    // So for every 40 shows that exist, we delay by 10s
+    // Could not use these in a loop since loops are synchronous
+    setTimeout(() => shows.slice(40, 80).map(show => getShow(show)), 10000);
+    setTimeout(() => shows.slice(80, 120).map(show => getShow(show)), 10000);
+    setTimeout(() => shows.slice(120, 160).map(show => getShow(show)), 10000);
+    setTimeout(() => shows.slice(160, 200).map(show => getShow(show)), 10000);
   });
 }
