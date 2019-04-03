f698af37955a32940864f3aabef47f9222a5e684
Sunjot Singh <sunjot@uoguelph.ca>
2019-04-03 02:56:02 -0400

    added overlay box to incl more show info

--- client/Components/ScheduleList.js
+++ client/Components/ScheduleList.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Close from 'react-feather/dist/icons/x-square';
 
 class ScheduleList extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      expandActive: 0
+    };
+  }
+
+  expandShow = () => {
+    this.setState({
+      expandActive: 1
+    });
+  }
+
+  closeShow = () => {
+    this.setState({
+      expandActive: 0
+    });
+  }
 
   render() {
     return(
@@ -9,7 +29,7 @@ class ScheduleList extends React.Component {
         {this.props.showData.map((show, x) => {
           return (
             <div key={x} className="sched-item">
-              <img src={"https://image.tmdb.org/t/p/w1280" + show.poster}/>
+              <img onClick={() => this.expandShow()} src={"https://image.tmdb.org/t/p/w1280" + show.poster}/>
               <div className="show-info">
                 <div className="name-date-cont">
                   <div className="show-name">{show.showName}</div>
@@ -17,6 +37,14 @@ class ScheduleList extends React.Component {
                 </div>
                 <div className="episode-name">{show.name}</div>
               </div>
+              {this.state.expandActive === 1  &&
+                <div id="expand-cont">
+                  <div id="expand-box">
+                    <Close id="close-icon" size={30} color="Black" onClick={() => this.closeShow()} />
+                  </div>
+                  <div id="expand-dim"></div>
+                </div>
+              }
             </div>
           )
         })}
